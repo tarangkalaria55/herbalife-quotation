f7f45299fec0081da395e7944b06310380f4d28e
Tarang Kalariya <tarangkalaria55@gmail.com>
2026-07-07 22:12:12 +0530

    $CG_MSG

---
+++ src/data/products.ts
@@ -0,0 +1,15 @@
+import type { IProduct } from './products.type';
+
+export const products: IProduct[] = [
+	{
+		productName: 'Product 1',
+		category: 'Category A',
+		vp: 10.25,
+		mrp: 100,
+		price15: 15,
+		price25: 25,
+		price35: 35,
+		price42: 42,
+		price50: 50,
+	},
+];
---
+++ src/data/products.type.ts
@@ -0,0 +1,11 @@
+export interface IProduct {
+	productName: string;
+	category: string;
+	vp: number;
+	mrp: number;
+	price15: number;
+	price25: number;
+	price35: number;
+	price42: number;
+	price50: number;
+}
